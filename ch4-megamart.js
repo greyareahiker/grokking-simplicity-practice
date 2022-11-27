1ac6554dc29abffc63cc6b8c79acb52442fe6e9b
jereint20 <jereint20@gmail.com>
2022-11-27 21:11:50 +0900

    :recycle: extract subroutine
    - 기존 코드에서 동작을 바꾸진 않았음
    - 70p

--- ch4-megamart.js
+++ ch4-megamart.js
@@ -28,12 +28,16 @@ function update_tax_dom() {
 }
 
 function calc_cart_total() {
+  calc_total()
+  set_cart_total_dom() // DOM action
+  update_shipping_icons()
+  update_tax_dom()
+}
+
+function calc_total() {
   shopping_cart_total = 0
   for (let i = 0; i < shopping_cart.length; i++) {
     const item = shopping_cart[i]
     shopping_cart_total += item.price
   }
-  set_cart_total_dom() // DOM action
-  update_shipping_icons()
-  update_tax_dom()
 }

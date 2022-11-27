6b0ca5d75728c3a5408212fc40e09550001025fd
jereint20 <jereint20@gmail.com>
2022-11-27 21:53:35 +0900

    :recycle: change calculation from subroutine
    - 암묵적 입출력을 명시적 입출력으로 변경
    - 71~72p

--- ch4-megamart.js
+++ ch4-megamart.js
@@ -28,16 +28,17 @@ function update_tax_dom() {
 }
 
 function calc_cart_total() {
-  calc_total()
+  shopping_cart_total = calc_total(shopping_cart)
   set_cart_total_dom() // DOM action
   update_shipping_icons()
   update_tax_dom()
 }
 
-function calc_total() {
-  shopping_cart_total = 0
-  for (let i = 0; i < shopping_cart.length; i++) {
-    const item = shopping_cart[i]
-    shopping_cart_total += item.price
+function calc_total(cart) {
+  let total = 0
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i]
+    total += item.price
   }
+  return total
 }

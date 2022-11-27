c92f5fc96bb71edfb219a04e41d4fb25ce987f14
jereint20 <jereint20@gmail.com>
2022-11-27 23:33:18 +0900

    :recycle: refact ch4 all megamart code

--- ch4-megamart.js
+++ ch4-megamart.js
@@ -7,6 +7,30 @@ function add_item_to_cart(name, price) {
   calc_cart_total()
 }
 
+function calc_cart_total() {
+  shopping_cart_total = calc_total(shopping_cart)
+  set_cart_total_dom() // DOM action
+  update_shipping_icons()
+  update_tax_dom()
+}
+
+// 61p 무료 배송비 계산
+function update_shipping_icons() {
+  const buy_buttons = get_buy_buttons_dom() // DOM get
+  for (let i = 0; i < buy_buttons.length; i++) {
+    const button = buy_buttons[i]
+    const item = button.item
+    if (is_free_shipping(item.price, shopping_cart_total))
+      button.show_free_shipping_icon() // DOM action
+    else button.hide_free_shipping_icon() // DOM action
+  }
+}
+
+// 62p 세금 계산
+function update_tax_dom() {
+  set_tax_dom(calc_tax(shopping_cart_total)) // DOM action
+}
+
 function add_item(cart, name, price) {
   const new_cart = cart.slice()
   new_cart.push({
@@ -16,28 +40,11 @@ function add_item(cart, name, price) {
   return new_cart
 }
 
-// 61p 무료 배송비 계산
-function update_shipping_icons() {
-  const buy_buttons = get_buy_buttons_dom() // DOM get
-  for (let i = 0; i < buy_buttons.length; i++) {
-    const button = buy_buttons[i]
-    const item = button.item
-    if (item.price + shopping_cart_total >= 20)
-      button.show_free_shipping_icon() // DOM action
-    else button.hide_free_shipping_icon() // DOM action
-  }
+function is_free_shipping(item_price, total) {
+  return item_price + total >= 20
 }
-
-// 62p 세금 계산
-function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.1) // DOM action
-}
-
-function calc_cart_total() {
-  shopping_cart_total = calc_total(shopping_cart)
-  set_cart_total_dom() // DOM action
-  update_shipping_icons()
-  update_tax_dom()
+function calc_tax(total) {
+  return total * 0.1
 }
 
 function calc_total(cart) {

c9241ac6b43e05cb1149499b37b4ab2b20d8eb26
jereint20 <jereint20@gmail.com>
2022-11-28 16:46:28 +0900

    :art: 99p add_item 분리

--- ch5-megamart.js
+++ ch5-megamart.js
@@ -2,7 +2,8 @@ const shopping_cart = []
 let shopping_cart_total = 0
 
 function add_item_to_cart(name, price) {
-  shopping_cart = add_item(shopping_cart, name, price)
+  const new_item = make_cart_item(name, price)
+  shopping_cart = add_item(shopping_cart, new_item)
   const total = calc_total(shopping_cart) // 오답노트: 암묵적 출력도 없앨 수 있으면 없애자
   set_cart_total_dom(total) // DOM action
   update_shipping_icons(shopping_cart)
@@ -13,7 +14,8 @@ function update_shipping_icons(cart) {
   const buy_buttons = get_buy_buttons_dom() // DOM get
   for (let i = 0; i < buy_buttons.length; i++) {
     const button = buy_buttons[i]
-    const new_cart = add_item(cart, item.name, item.price)
+    const item = button.item
+    const new_cart = add_item(cart, item)
     if (is_free_shipping(new_cart))
       button.show_free_shipping_icon() // DOM action
     else button.hide_free_shipping_icon() // DOM action
@@ -23,13 +25,16 @@ function update_shipping_icons(cart) {
 function update_tax_dom(total) {
   set_tax_dom(calc_tax(total)) // DOM action
 }
-// C I
-function add_item(cart, name, price) {
+
+// I
+function make_cart_item(name, price) {
+  return { name, price }
+}
+
+// C
+function add_item(cart, item) {
   const new_cart = cart.slice()
-  new_cart.push({
-    name: name,
-    price: price,
-  })
+  new_cart.push(item)
   return new_cart
 }
 // C I B

9e9a76725aa38ba128fd67325dc47e24a86463af
jereint20 <jereint20@gmail.com>
2022-11-29 10:23:08 +0900

    :recycle: 113~117p copy on write(COW) 적용

--- ch6-remove_item_by_name.js
+++ ch6-remove_item_by_name.js
@@ -5,3 +5,34 @@ function remove_item_by_name(cart, name) {
   }
   if (idx !== null) cart.splice(idx, 1)
 }
+
+// step1 복사본 만들기
+function remove_item_by_name(cart, name) {
+  const new_cart = cart.slice()
+  let idx = null
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name === name) idx = i
+  }
+  if (idx !== null) cart.splice(idx, 1)
+}
+
+// step2 복사본 변경하기 -> 기존 cart 를 사용하는 부분을 복사본으로 변경
+function remove_item_by_name(cart, name) {
+  const new_cart = cart.slice()
+  let idx = null
+  for (let i = 0; i < new_cart.length; i++) {
+    if (new_cart[i].name === name) idx = i
+  }
+  if (idx !== null) new_cart.splice(idx, 1)
+}
+
+// step3 복사본 리턴하기
+function remove_item_by_name(cart, name) {
+  const new_cart = cart.slice()
+  let idx = null
+  for (let i = 0; i < new_cart.length; i++) {
+    if (new_cart[i].name === name) idx = i
+  }
+  if (idx !== null) new_cart.splice(idx, 1)
+  return new_cart
+}

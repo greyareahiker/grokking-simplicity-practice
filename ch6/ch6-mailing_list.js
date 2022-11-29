ff39229f07b458ced563afe62659aa190cc3f52e
jereint20 <jereint20@gmail.com>
2022-11-30 01:02:37 +0900

    :recycle: 121p 연습문제 풀이 (COW 적용)

--- ch6/ch6-mailing_list.js
+++ ch6/ch6-mailing_list.js
@@ -1,11 +1,13 @@
-const mailing_list = []
+let mailing_list = []
 
-function add_contact(email) {
-  mailing_list.push(email)
+function add_contact(list, email) {
+  const list_copy = list
+  list_copy.push(email)
+  return list_copy
 }
 
 function submit_form_handler(event) {
   const form = event.target
   const email = form.elements["email"].value
-  add_contact(email)
+  mailing_list = add_contact(mailing_list, email)
 }

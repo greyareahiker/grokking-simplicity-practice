42815671277035d8cdc949969d645b7b2cd071ae
jereint20 <jereint20@gmail.com>
2022-12-24 03:10:27 +0900

    :recycle: 438p 연습문제 풀이

--- ch15/ch15-doDishes.js
+++ ch15/ch15-doDishes.js
@@ -2,21 +2,20 @@
  * const plates = ...;
  * const forks = ...;
  * const cups = ...;
- * const total = ...;
  */
 
-function doDishes() {
-  total = 0
+function doDishes(plates, forks, cups, cb) {
+  const total = 0
   wash_ajax(plates, function () {
     total += plates.length
     wash_ajax(forks, function () {
       total += forks.length
       wash_ajax(cups, function () {
         total += cups.length
-        update_dishes_dom(total)
+        cb(total)
       })
     })
   })
 }
 
-doDishes()
+doDishes(plates, forks, cups, update_dishes_dom)
